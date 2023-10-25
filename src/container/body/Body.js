bfdedc3309e06f321c16bb3f8babe434ac58812d
jishnunpp <jishnunp397@gmail.com>
2023-10-25 13:46:57 +0530

    brand cards added

--- src/container/body/Body.js
+++ src/container/body/Body.js
@@ -2,25 +2,23 @@ import React from 'react'
 import body from '../../assets/body.avif'
 
 import './body.css'
-import Topbar from '../../components/topbar/Topbar'
-import Navbar from '../../components/navbar/Navbar'
-import Menubar from '../../components/menubar/Menubar'
 import Carosuel_card from '../../components/carousel_card/Carosuel_card'
+import Header from '../header/Header'
+import Card from '../../components/Brand_card/Card'
 
 
 const Body = () => {
   return (
     <div className='body'>
-        <Topbar/>
-        <Navbar/>
-        <Menubar/>
+        <Header/>
         <Carosuel_card/>
+        <Card/>
         
         
     
-        <div className='body-image'>
+        {/* <div className='body-image'>
          <img src={body} alt='img'/>
-        </div>
+        </div> */}
         <div>
 
         </div>
